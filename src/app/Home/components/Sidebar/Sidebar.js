b59e96860b72134e55b7610e556551beed7b7215
juanpablopos <juan_posada82211@elpoli.edu.co>
2024-12-03 09:13:50 -0500

    as

--- src/app/Home/components/Sidebar/Sidebar.js
+++ src/app/Home/components/Sidebar/Sidebar.js
@@ -1,7 +1,8 @@
 'use client'
-import { FaCalculator, FaBox } from 'react-icons/fa';
 import { useRouter } from 'next/navigation';
 import { BiLogOut } from "react-icons/bi";
+import { MdShoppingCart } from "react-icons/md";
+import { FaBoxOpen } from "react-icons/fa";
 
 const Sidebar = ({isOpen, setIsOpen}) => {
  
@@ -11,11 +12,11 @@ const Sidebar = ({isOpen, setIsOpen}) => {
         <div className="bg-[#3C3E6C] text-white h-screen w-20 flex flex-col justify-between items-center">
             <div className='w-full h-full max-h-[400px] text-xl flex flex-col items-center justify-start'>
                 <div className="w-full h-24 flex items-center justify-center hover:bg-white hover:text-[#54307f]  cursor-pointer">
-                    logo
+                    <MdShoppingCart className='text-white text-4xl hover:text-[#3C3E6C]'/>
                 </div>
                 <div title='Productos' className='w-full h-20   ' >
                     <div onClick={()=>setIsOpen(1)}  className={`w-full h-full flex items-center justify-center hover:transition-all duration-300 hover:bg-white cursor-pointer hover:text-[#54307f] ${isOpen === 3 ? 'bg-slate-50 text-[#54307f]' : ''}`}>
-                        <FaBox />
+                        <FaBoxOpen className='text-white text-4xl hover:text-[#3C3E6C]'/>
                     </div>
                 </div>
             </div>
